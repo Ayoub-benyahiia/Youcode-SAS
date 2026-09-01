0729b4b1bf12f158e307e4dc3f3ad4dfcf454feb
Ayoub Ben Yahia <ayoubayoubsh20@gmail.com>
2026-09-01 09:55:53 +0100

    challenge 1

---
+++ Challenge/chalenge.js
@@ -0,0 +1,15 @@
+// Exercices sur les Variables et les Opérateurs
+// Challenge 1 : Profil d'un joueur
+const Pseudo = "ShadowX"
+let Niveau = "12"
+let Age = "21"
+let score = "4580"
+const pays = "Maroc"
+console.log(Pseudo);
+console.log(Niveau);
+console.log(Age);
+console.log(score);
+console.log(pays);
+
+
+// Challenge 2 : Conversion de monnaie
