7466f834d76c7720ece70338c965ad9aa7da030f
Ayoub Ben Yahia <ayoubayoubsh20@gmail.com>
2026-09-03 09:52:57 +0100

    update

--- Day_1/Boucles/challenge-1.js
+++ Day_1/Boucles/challenge-1.js
@@ -1,3 +1,7 @@
 //Partie 1 — Boucle for
 //Challenge 1 : Compter les participants
 var prompt = require('prompt-sync')();
+var n = prompt('combien de participation? ');
+for (i=1;i<=n;i++){
+    console.log(i)
+}
