13c2541991fbaef901c43446de112f99b9ee34da
Ayoub Ben Yahia <ayoubayoubsh20@gmail.com>
2026-09-03 11:44:20 +0100

    update

---
+++ Day_1/Boucles/challenge-6.js
@@ -0,0 +1,9 @@
+// Partie 2 — Boucle while
+// Challenge 6 : Compte à rebours
+var prompt = require('prompt-sync')();
+var n = Number(prompt('Donner N : '));
+let i = 5;
+while (i >= 0) {
+    console.log(i);
+    i--;
+};
