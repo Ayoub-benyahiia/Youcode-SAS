69ddc975862562f82e85e0e7aae66d8d1f9dbd39
Ayoub Ben Yahia <ayoubayoubsh20@gmail.com>
2026-09-03 15:02:50 +0100

    update

---
+++ Day_1/Boucles/challenge-7.js
@@ -0,0 +1,8 @@
+// Challenge 7 : Trouver le premier nombre supérieur
+// var prompt = require('prompt-sync')();
+// var n = Number(prompt('Donner N : '));
+let i = 1;
+while (i * i <= 100) {
+    i++;
+}
+console.log(i);
