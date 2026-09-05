35dbeddf9efd6b6f9b0374fb0f160dc45b3f8953
Ayoub Ben Yahia <ayoubayoubsh20@gmail.com>
2026-09-05 22:26:08 +0100

    update

--- Day_1/tableaux/challenge-3.js
+++ Day_1/tableaux/challenge-3.js
@@ -1,1 +1,14 @@
 // Challenge 3 : Recherche du Maximum
+array = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+//let Maximum = array[0]
+// let numberofelements = array[i]
+function trouverMax(tab) {
+    let Maximum = tab[0]
+    for (i = 0; i in array; i++) {
+        if (array[i] > Maximum)
+            Maximum = array[i];
+    }
+    return Maximum
+}
+output = trouverMax(array);
+console.log(output)
