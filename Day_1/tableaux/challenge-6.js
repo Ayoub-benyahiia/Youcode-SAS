f8844ff8df73ba8daf03b42a8975495fb6ff5758
Ayoub Ben Yahia <ayoubayoubsh20@gmail.com>
2026-09-06 19:00:03 +0100

    update

--- Day_1/tableaux/challenge-6.js
+++ Day_1/tableaux/challenge-6.js
@@ -1,31 +1,17 @@
 // Challenge 6 : Inversion Manuelle (Simulation de reverse)
 
-const tableau1 =[10,20,30,40,"rachid","mohammed"]
-let tableau2 = tableau1[tableau1.length -1]
 
-function inverserTableau(tab){
-    for(let i=0; i <=tab; i++)
-        console.log(tab[i])
-        return tab
+
+const array1 = [10, 20, 30, 40, "rachid", "mohammed"]
+function reversTable(array) {
+  let j = array.length - 1;
+  for (i = 0; i < j; i++) {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    j--;
+  }
+  return array
 }
-
-inverserTableau(tableau1);
-console.log(inverserTableau(tableau1));
-console.log(tableau2);
-
-function glebch9leb(tab){
-    let i= -1
-    for(i in tab)
-        i++;
-}
-glebch9leb(tableau1)
-
-
-
-function reverse(tab) {
-var output = '';
-for (var i = tab.length - 1; i >= 0; i--)
-  output += tab[i];
-return output;
-}
-onsole.log(reverseString("youcode"));
+let  revers =reversTable(array1);
+console.log(revers);
