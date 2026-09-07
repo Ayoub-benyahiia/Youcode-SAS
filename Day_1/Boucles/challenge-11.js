71802cfb2345fe62cbfff4c155e80521aeac14ce
Ayoub Ben Yahia <ayoubayoubsh20@gmail.com>
2026-09-07 22:46:42 +0100

    finish this exercice Challenge 12 : Compter les diviseurs

--- Day_1/Boucles/challenge-11.js
+++ Day_1/Boucles/challenge-11.js
@@ -1,2 +1,26 @@
-// Partie 4 — Boucle while
-// Challenge 16 : Deviner un nombre
+// Partie 3 — Boucle for 
+//Challenge 12 : Compter les diviseurs
+
+// Nombre : 12
+
+Diviseurs:
+1
+2
+3
+4
+6
+12
+
+// Nombre de diviseurs : 6
+
+const prompt = require('prompt-sync')()
+let N = +prompt("entre your nombre : ")
+
+let count = 0;
+for (let i = 1; i <= N; i++) {
+    if (N % i == 0) {
+        count++;
+        console.log("Diviseurs :" + i)
+    }
+}
+console.log("Nombre de diviseurs " +count)
